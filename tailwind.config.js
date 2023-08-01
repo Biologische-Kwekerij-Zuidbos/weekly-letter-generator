7c07eddb0251e0da6c2455ecdbc74bd54839b480
Casper Aangeenbrug <casper.aangeenbrug@omoda.nl>
2023-08-01 10:50:35 +0200

    npm run format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,jsx,tsx}'],
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [require("daisyui")],
   daisyui: {
-    themes: ['light'],
+    themes: ["light"],
   },
 }
