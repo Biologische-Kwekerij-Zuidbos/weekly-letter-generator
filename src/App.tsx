4efb6b20ba4f06a07deabeee9848e1979938d85c
Casper Aangeenbrug <casper.aangeenbrug@omoda.nl>
2023-08-01 10:24:25 +0200

    Install daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import './App.css'
-
 function App() {
-  return <h1>test</h1>
+  return (
+    <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+      Button
+    </button>
+  )
 }
 
 export default App
